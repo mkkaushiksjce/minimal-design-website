40dd2a33677155273226b38344b07289aadb8420
Kaushik M K <mkkaushiksjce@gmail.com>
2017-09-12 21:11:19 +0530

    smooth scrolling fixed

--- app/js/app.js
+++ app/js/app.js
@@ -4,6 +4,11 @@ var bgScrollParams = {
     currentPosition: 0,
     scrollDirection: "horizontal"
 }
+var servicePadding;
+var contactPadding;
+var offset;
+var currScroll = $(this).scrollTop();
+var scrollDirection = 'bottom';
 
 $(document).ready(function () {
 
@@ -13,26 +18,44 @@ $(document).ready(function () {
         }, scrollDelay);
     });
 
+
     $("#service").click(function () {
+        servicePadding = parseInt($(".services-section-wrap").css('padding-bottom').match(/\d+/)[0]);
+
+        currScroll = $(this).scrollTop();
+
+        if (currScroll > lastScrollTop) {
+            scrollDirection = 'bottom';
+             offset = $(".services-section-wrap").offset().top - servicePadding ;
+            //  offset = $(".services-section-wrap").offset().top - servicePadding - $(".navbar-wrap").height();
+        } else {
+            scrollDirection = 'top';
+            offset = $(".services-section-wrap").offset().top - servicePadding;
+        }
+        lastScrollTop = currScroll;
+
         $("html, body").animate({
-            scrollTop: $(".services-section-wrap").offset().top
+            scrollTop: offset
         }, scrollDelay);
     });
 
     $("#contact, #contactBtn").click(function () {
+        contactPadding = parseInt($(".contactus-section-wrap").css('padding-bottom').match(/\d+/)[0]);
+        // scrollTop: $(".contactus-section-wrap").offset().top - $(".navbar-wrap").height() - contactPadding
+        // scrollTop: $(".contactus-section-wrap").offset().top - contactPadding
         $("html, body").animate({
-            scrollTop: $(".contactus-section-wrap").offset().top
+            scrollTop: $(".contactus-section-wrap").offset().top - $(".navbar-wrap").height() - contactPadding
         }, scrollDelay);
     });
 
-    function backgroundScroll(bgScrollParams){
+    function backgroundScroll(bgScrollParams) {
         // 1 pixel row at a time
         bgScrollParams.current -= 1;
-   
+
         // move the background with backgrond-position css properties
-        $('div.clouds').css("backgroundPosition", (bgScrollParams.direction == 'horizontal') ? bgScrollParams.current+"px 0" : "0 " + bgScrollParams.current+"px");
+        $('div.clouds').css("backgroundPosition", (bgScrollParams.direction == 'horizontal') ? bgScrollParams.current + "px 0" : "0 " + bgScrollParams.current + "px");
     }
 
     //Calls the scrolling function repeatedly
-     setInterval(bgscroll, scrollSpeed);  
+    //  setInterval(bgscroll, scrollSpeed);  
 });
--- app/js/scroll.js
+++ app/js/scroll.js
@@ -1,4 +1,7 @@
 var headerTop = 50;
+var navbarFixed = false;
+var activeElm = null;
+var lastScrollTop = 0;
 
 function getCurrentScroll() {
     return window.pageYOffset || document.documentElement.scrollTop;
@@ -9,7 +12,23 @@ $(window).scroll(function () {
     var scroll = getCurrentScroll();
     if (scroll >= headerTop) {
         $('.navbar-wrap').addClass('header-fixed');
+        navbarFixed = true;
     } else {
         $('.navbar-wrap').removeClass('header-fixed');
+        navbarFixed = false;
     }
 });
+
+$(".nav-component").on('click', function(){
+    $(".nav-component").each(function(element) {
+        if(this.className.indexOf('active-nav') != -1){
+            $(this).removeClass("active-nav");
+        };
+    }, this);
+    activeElm = $(this)[0].innerText
+    $(this).addClass('active-nav');
+});
+
+
+
+
